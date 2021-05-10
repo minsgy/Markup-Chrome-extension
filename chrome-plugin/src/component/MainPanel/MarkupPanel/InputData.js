c736184a0377bd272de9d26c33d34457ea908613
minsgy <alstjrchl97@naver.com>
2021-05-10 23:50:08 +0900

    HTML/CSS 파일 입력

--- chrome-plugin/src/component/MainPanel/MarkupPanel/InputData.js
+++ chrome-plugin/src/component/MainPanel/MarkupPanel/InputData.js
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import styled from 'styled-components';
 
 const FileBox = styled.form`
@@ -38,40 +38,36 @@ const CheckButton = styled.button`
 `;
 
 const InputData = () => {
+
     const HTMLRef = useRef(null);
     const CSSRef = useRef(null);
 
+    const [HTMLText, SetHTMLText] = useState("");
+    const [CSSText, SetCSSText] = useState("");
+
+    // HTMLFILE 읽기
+    const handleChangeFile = (e) => {
+        let reader = new FileReader();
+
+        if (e.target.files[0]){
+            reader.readAsText(e.target.files[0], "UTF-8");
+            reader.onload = () => {
+                console.log(reader.result);
+            }
+        }
+    }
     
 
     return (
             <>
                 <FileContainer>
-                    <Markup_Label for="HTML_file">HTML 업로드</Markup_Label>
-                    <Markup_Label for="CSS_file">CSS 업로드</Markup_Label>
-                    <Markup_File ref={HTMLRef} type="file" id="HTML_file" />
-                    <Markup_File ref={CSSRef} type="file" id="CSS_file" />
+                    <Markup_Label htmlFor="HTML_file">HTML 업로드</Markup_Label>
+                    <Markup_Label htmlFor="CSS_file">CSS 업로드</Markup_Label>
+                    <Markup_File ref={HTMLRef} onChange={handleChangeFile} type="file" accept="text/html" id="HTML_file" />
+                    <Markup_File ref={CSSRef} onChange={handleChangeFile} type="file" accept="text/css"  id="CSS_file" />
                 </FileContainer>
-                <CheckButton type="submit" onClick="">체크하기</CheckButton>
+                <CheckButton type="submit">체크하기</CheckButton>
            </>
-        // <Form>
-        //     <FormContainer>
-        //         <Form.Group>
-        //             <Form.Label>HTML Code</Form.Label>
-        //             <Form.Control as="textarea" rows={3} required/>
-        //             <Form.Text>
-        //                 CSS가 적용 될 HTML을 입력해주세요.
-        //             </Form.Text>
-        //         </Form.Group>
-        //         <Form.Group>
-        //             <Form.Label>CSS Code</Form.Label>
-        //             <Form.Control as="textarea" rows={3} required/>
-        //             <Form.Text>
-        //                 CSS가 적용 될 HTML을 입력해주세요.
-        //             </Form.Text>
-        //         </Form.Group>
-        //     </FormContainer>
-        //     <Button variant="primary" type="submit">검사하기</Button>
-        // </Form>
     );
 }
 
